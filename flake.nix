b0e39e12e94b4b4af2fcffc0da0055160fceb1fb
NEDOSTUPN0 <v.ya.1910@gmail.com>
2026-09-23 18:29:45 +0200

    chore: license under MIT

--- flake.nix
+++ flake.nix
@@ -15,7 +15,10 @@
           version = "0.1.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
-          meta.mainProgram = "rechenkern";
+          meta = {
+            mainProgram = "rechenkern";
+            license = pkgs.lib.licenses.mit;
+          };
         };
       });
 
