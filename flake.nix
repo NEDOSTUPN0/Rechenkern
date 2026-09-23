f1f414dbdf363b140d422d0369017f9f07bb2c9f
NEDOSTUPN0 <v.ya.1910@gmail.com>
2026-09-23 19:00:24 +0200

    chore: release 0.1.1

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       packages = forAll (pkgs: {
         default = pkgs.rustPlatform.buildRustPackage {
           pname = "rechenkern";
-          version = "0.1.0";
+          version = "0.1.1";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
           meta = {
